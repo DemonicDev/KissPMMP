9d8715cce945ce57761c62718b03131c649e01e6
Tijmen Vos <61244099+DemonicDev@users.noreply.github.com>
2026-03-11 20:06:01 +0000

    Create main plugin class for kisspmmp
    Implement main plugin class for kisspmmp.

---
+++ src/kisspmmp/main.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace kisspmmp;
+
+use pocketmine\plugin\PluginBase;
+
+class main extends PluginBase{
+
+  public function onEnable(): void{
+    //this version is coded on Github Web Editor, so untested
+    $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+  }
+}
