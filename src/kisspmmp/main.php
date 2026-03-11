61eb9627513f866deb704b6373fe4bc5541d3b40
Tijmen Vos <61244099+DemonicDev@users.noreply.github.com>
2026-03-11 20:46:27 +0000

    Pass 'this' to EventListener during registration

--- src/kisspmmp/main.php
+++ src/kisspmmp/main.php
@@ -8,6 +8,8 @@ class main extends PluginBase{
 
   public function onEnable(): void{
     //this version is coded on Github Web Editor, so untested
-    $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+    $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
   }
+
+  public function kissAction($player, $ps, $world){} //todo
 }
