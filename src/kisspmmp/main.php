0292f6b2631b573d043d451ad65d1f14b3ab0fbb
Tijmen Vos <61244099+DemonicDev@users.noreply.github.com>
2026-03-12 21:10:37 +0000

    Add HeartParticle effect in kissAction method

--- src/kisspmmp/main.php
+++ src/kisspmmp/main.php
@@ -3,6 +3,7 @@
 namespace kisspmmp;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\world\particle\HeartParticle;
 
 class main extends PluginBase{
 
@@ -11,5 +12,9 @@ class main extends PluginBase{
     $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
   }
 
-  public function kissAction($player, $ps, $world){} //todo
+  public function kissAction($player, $ps, $world){
+    $particle = new HeartParticle();
+    $pos = $player->getPosition()->asVector3(); //make later custom by config
+    $world->addParticle($pos, $particle);
+  } //todo
 }
